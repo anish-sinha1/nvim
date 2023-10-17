77cf4e37c81907d4a40f5e6b2e4696b56edc16d7
Anish Sinha <anishsinha0128@gmail.com>
2023-10-17 00:45:16 -0400

    Update ToggleTerm keymaps to make it easier to rename, open, and toggle between terminals

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,14 @@
 -- Lua
 
 vim.g.maplocalleader = ";"
+
+-- Glance
 vim.keymap.set("n", "<leader>gd", "<CMD>Glance definitions<CR>")
 vim.keymap.set("n", "<leader>gr", "<CMD>Glance references<CR>")
 vim.keymap.set("n", "<leader>gy", "<CMD>Glance type_definitions<CR>")
 vim.keymap.set("n", "<leader>gm", "<CMD>Glance implementations<CR>")
+
+-- ToggleTerm
+vim.keymap.set("n", "<leader>ra", "<CMD>ToggleTermToggleAll<CR>")
+vim.keymap.set("n", "<leader>rs", "<CMD>TermSelect<CR>")
+vim.keymap.set("n", "<leader>rn", "<CMD>ToggleTermSetName<CR>")
