bc45539524555d1bcae05192d2d59386dc73544b
Anish Sinha <anishsinha0128@gmail.com>
2023-10-21 12:45:19 -0400

    Uncomment neogen config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -97,13 +97,13 @@ require("lazy").setup({
       end,
     },
 
-    -- {
-    --   "danymat/neogen",
-    --   dependencies = "nvim-treesitter/nvim-treesitter",
-    --   config = true,
-    --   -- Uncomment next line if you want to follow only stable versions
-    --   -- version = "*"
-    -- },
+    {
+      "danymat/neogen",
+      dependencies = "nvim-treesitter/nvim-treesitter",
+      config = true,
+      -- Uncomment next line if you want to follow only stable versions
+      -- version = "*"
+    },
 
     { import = "plugins" },
   },
